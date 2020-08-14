890d63f3be54b1f5a7c85446f3bbf363d05ad0c8
Vitalii Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-08-14 15:00:19 +0300

    Add create user implementation

--- src/main/java/com/appsdeveloperblog/photoapp/api/users/users/service/UsersServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/users/service/UsersServiceImpl.java
@@ -1,14 +1,34 @@
 package com.appsdeveloperblog.photoapp.api.users.users.service;
 
+import com.appsdeveloperblog.photoapp.api.users.users.data.UserEntity;
+import com.appsdeveloperblog.photoapp.api.users.users.data.UsersRepository;
 import com.appsdeveloperblog.photoapp.api.users.users.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+import static org.modelmapper.convention.MatchingStrategies.STRICT;
+
+@Service
 public class UsersServiceImpl implements UsersService {
+
+    UsersRepository usersRepository;
+
+    @Autowired
+    public UsersServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
 
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
-        return null;
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(STRICT);
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPassword("test");
+        usersRepository.save(userEntity);
+        return userDto;
     }
 }
--- src/main/java/com/appsdeveloperblog/photoapp/api/users/users/ui/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/users/users/ui/controllers/UsersController.java
@@ -1,13 +1,22 @@
 package com.appsdeveloperblog.photoapp.api.users.users.ui.controllers;
 
+import com.appsdeveloperblog.photoapp.api.users.users.service.UsersService;
+import com.appsdeveloperblog.photoapp.api.users.users.shared.UserDto;
 import com.appsdeveloperblog.photoapp.api.users.users.ui.model.CreateUserRequestModel;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+import static org.modelmapper.convention.MatchingStrategies.STRICT;
+
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UsersController {
+
+    private final UsersService usersService;
 
     @GetMapping("/status/check")
     public String status() {
@@ -16,6 +25,10 @@ public class UsersController {
 
     @PostMapping
     public String createUser(@Valid  @RequestBody CreateUserRequestModel userDetails) {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(STRICT);
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+        usersService.createUser(userDto);
         return "Create user method is called";
     }
 
